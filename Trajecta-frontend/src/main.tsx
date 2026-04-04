2124ff701e48da6d55c04877b4d86b706dae2a69
Jackstaffgg <demyanenko.ad@gmail.com>
2026-04-04 14:51:55 +0300

    fixes

--- Trajecta-frontend/src/main.tsx
+++ Trajecta-frontend/src/main.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Cesium from "cesium";
 import App from "@/App";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "@/styles.css";
+
+const globalScope = globalThis as typeof globalThis & {
+  Cesium?: typeof Cesium;
+  CESIUM_BASE_URL?: string;
+};
+
+const baseUrl = (import.meta.env.BASE_URL || "/").replace(/\/?$/, "/");
+globalScope.Cesium = Cesium;
+globalScope.CESIUM_BASE_URL = `${baseUrl}cesium/`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
